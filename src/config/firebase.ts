3d8b57e2b37611ed301975f750684f52115912df
Anuj <anupta45@gmail.com>
2024-08-05 09:02:26 +0530

    feat: firebase env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,9 +6,9 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAkFNDQFeigsTtciNEgE0LAfoliRdFB1Pw',
-  authDomain: 'ava-chatbot-f2551.firebaseapp.com',
-  projectId: 'ava-chatbot-f2551',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
 };
 
 // Initialize Firebase
